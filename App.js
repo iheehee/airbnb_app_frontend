b082227ad4feaf55ef5be3c273225769e8342ecd
iheehee <iheeheedev@gmail.com>
2023-02-02 17:52:05 +0900

    연습

--- App.js
+++ App.js
@@ -1,6 +1,46 @@
 import React, { useState } from "react";
-import AppLoading from "expo-app-loading";
-import { Asset } from "expo-asset";
+import AppLoading  from "expo-app-loading";
+import { Asset } from 'expo-asset';
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, Image } from "react-native";
+
+const cacheImanges = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+
+const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  const handleFinish = () => setIsReady(true);
+  const loadAssets = async () => {
+    const images = [
+      require("./assets/Airbnb.jpeg"),
+      "https://news.airbnb.com/wp-content/uploads/sites/4/2017/07/airbnb-newsroom-twitter-card-default.png?fit=2400%2C1260"
+    ];
+  
+    const fonts = [Ionicons.font];
+    cacheImanges(images);
+    console.log(cacheFonts(fonts));
+  };
+  return isReady ? (
+    <Text>I'm ready</Text>
+  ) : (
+    <AppLoading
+      onError={console.error}
+      onFinish={handleFinish}
+      startAsync={loadAssets}
+    />
+  );
+}
+
+/* import { Asset } from "expo-asset";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Text, Image } from "react-native";
@@ -38,4 +78,4 @@ export default function App() {
       startAsync={loadAssets}
     />
   );
-}
+} */
