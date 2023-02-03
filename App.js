430ec6a6154f338113383aa4cf54dfe11218d439
iheehee <iheeheedev@gmail.com>
2023-02-03 16:10:02 +0900

    게이트 컴포넌트 빌드

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import AppLoading  from "expo-app-loading";
-import { Asset } from 'expo-asset';
+import AppLoading from "expo-app-loading";
+import { Asset } from "expo-asset";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Text, Image } from "react-native";
+import Gate from "./components/Gate";
 
-const cacheImanges = (images) =>
+const cacheImages = (images) =>
   images.map((image) => {
     if (typeof image === "string") {
       return Image.prefetch(image);
@@ -22,15 +23,17 @@ export default function App() {
   const loadAssets = async () => {
     const images = [
       require("./assets/Airbnb.jpeg"),
-      "https://news.airbnb.com/wp-content/uploads/sites/4/2017/07/airbnb-newsroom-twitter-card-default.png?fit=2400%2C1260"
+      "https://news.airbnb.com/wp-content/uploads/sites/4/2017/07/airbnb-newsroom-twitter-card-default.png?fit=2400%2C1260",
     ];
-  
+
     const fonts = [Ionicons.font];
-    cacheImanges(images);
-    console.log(cacheFonts(fonts));
+    const imagePromises = cacheImages(images);
+    const fontPromises = cacheFonts(fonts);
+    
+    return Promise.all([...fontPromises, ...imagePromises]);
   };
   return isReady ? (
-    <Text>I'm ready</Text>
+    <Gate />
   ) : (
     <AppLoading
       onError={console.error}
