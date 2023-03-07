f51e380b06741ce049016c5f0e4bf315ffb65747
iheehee <iheeheedev@gmail.com>
2023-03-07 15:16:20 +0900

    fetch data

--- api.js
+++ api.js
@@ -16,5 +16,6 @@ const callApi = async (method, path, data, jwt) => {
 
 export default {
   createAccount: form => callApi("post", "/users/", form),
-  login: form => callApi("post", "/users/login/", form)
+  login: form => callApi("post", "/users/login/", form),
+  rooms: (page = 1) => callApi("get", `/rooms/?page=${page}`)
 };
---
+++ screens/Main/Explore/ExploreContainer.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+import ExplorePresenter from "./ExplorePresenter";
+
+export default ({ getRooms, rooms, page }) => {
+    useEffect(() => {
+      getRooms();
+    }, []);
+    return <ExplorePresenter rooms={rooms} />;
+  };
+  
