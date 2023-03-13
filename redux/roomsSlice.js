43b954376936b0de17ea1c5cbd7e66010747ccd8
iheehee <iheeheedev@gmail.com>
2023-03-13 12:42:51 +0900

    more room

--- redux/roomsSlice.js
+++ redux/roomsSlice.js
@@ -1,3 +1,5 @@
+
+
 import { createSlice } from "@reduxjs/toolkit";
 import api from "../api";
 
@@ -12,8 +14,12 @@ const roomsSlice = createSlice({
   },
   reducers: {
     setExploreRooms(state, action) {
-        const { explore } = state;
-        const { payload } = action;
+      const { explore } = state;
+      const { payload } = action;
+      if (payload.page === 1) {
+        state.explore.rooms = payload.rooms;
+        state.explore.page = 1;
+      } else {
         payload.rooms.forEach(payloadRoom => {
           const exists = explore.rooms.find(
             savedRoom => savedRoom.id === payloadRoom.id
@@ -22,26 +28,30 @@ const roomsSlice = createSlice({
             explore.rooms.push(payloadRoom);
           }
         });
-        state.explore.page = payload.page;
+      }
+    },
+    increasePage(state, action) {
+      state.explore.page += 1;
     }
   }
 });
 
-const { setExploreRooms } = roomsSlice.actions;
+export const { setExploreRooms, increasePage } = roomsSlice.actions;
 
-export const getRooms = () => async dispatch => {
-    try {
-      const {
-        data: { results }
-      } = await api.rooms();
-      dispatch(
-        setExploreRooms({
-          rooms: results,
-          page: 1
-        })
-      );
-    } catch (e) {}
-  };
-  
+export const getRooms = page => async dispatch => {
+  try {
+    const {
+      data: { results }
+    } = await api.rooms(page);
+    dispatch(
+      setExploreRooms({
+        rooms: results,
+        page
+      })
+    );
+  } catch (e) {
+    console.warn(e);
+  }
+};
 
 export default roomsSlice.reducer;
--- screens/Main/Explore/ExploreContainer.js
+++ screens/Main/Explore/ExploreContainer.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ExplorePresenter from "./ExplorePresenter";
 
-export default ({ getRooms, rooms, page }) => {
-    useEffect(() => {
-      getRooms(page);
-    }, [page]);
-    return <ExplorePresenter rooms={rooms} />;
-  };
-  
+export default ({ getRooms, rooms, increasePage, page }) => {
+  useEffect(() => {
+    getRooms(1);
+  }, []);
+  useEffect(() => {
+    getRooms(page);
+  }, [page]);
+  return <ExplorePresenter rooms={rooms} increasePage={increasePage} />;
+};
